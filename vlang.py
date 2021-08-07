e28930c79ee18dca96860c875c9fcdc1c4182887
abhi <abhiniveshs.mail@gmail.com>
2021-08-07 15:28:46 +0530

    variables and disp added

--- vlang.py
+++ vlang.py
@@ -9,7 +9,10 @@ file_name = sys.argv[1]
 file = open(file_name, "r")
 content = file.read()
 
-que = deque()
+string_var = deque()
+var_var = deque()
+var_data = deque()
+
 
 # lexical and parsing unit
 i = 0
@@ -18,11 +21,43 @@ while i < len(content):
         j = i
         while (content[j] != ">"):
             j += 1
-        que.appendleft(content[i+1:j])
+        string_var.appendleft(content[i+1:j])
+        i = j 
+    if (content[i] == "$"):
+        j = i 
+        while (content[j] !="_"):
+            j += 1
+        var_var.appendleft(content[i+1:j])
         i = j
-    elif (content[i] == "#"):
-        print(que[0])
+    
+
+    if (content[i] == "#"):
+         var_book = {var_var[0]:var_data[0]}
+         #print(var_book[var_var[0]])
+
+    if (content[i] == "="):
+        j = i 
+        if (content[i+2]=="<"):
+            while (content[j] !=">"):
+                j += 1
+        var_data.appendleft(content[i+3:j])
+        i = j 
+    
+    if (content[i:i+4] == "disp"):
+        j = i 
+        while (content[j] != "_"):
+            j += 1
+        #print(content[i+5:])
+        try:
+            print(var_book[content[i+6:j]])
+        except:
+            pass
     i+=1
+
+
+
+
+
 
 '''
 SYNTAX (v0.1)
@@ -34,5 +69,5 @@ for vlang version 0.1
 	4. Example: <Velalar>#
 	5. # - it indicates end of a line, 
     6. every line of vlang must contain # at its end
-
+    7. variable starts with $ and ends with _
 '''
